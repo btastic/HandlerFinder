7f10662d9eb82ec810bfd160a73f9339d82ea448
Ben Konsemüller <bkonsem@gmail.com>
2021-11-20 17:12:03 +0100

    First test version

--- HandlerFinder/FindHandlerCommand.cs
+++ HandlerFinder/FindHandlerCommand.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
-using System.Globalization;
+using System.Linq;
 using System.Threading.Tasks;
+using EnvDTE;
+using EnvDTE80;
 using Microsoft;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.LanguageServices;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Threading;
 using Document = Microsoft.CodeAnalysis.Document;
+using Project = Microsoft.CodeAnalysis.Project;
+using Solution = Microsoft.CodeAnalysis.Solution;
 using Task = System.Threading.Tasks.Task;
 
 namespace HandlerFinder
@@ -31,12 +37,12 @@ namespace HandlerFinder
         /// <summary>
         /// Command menu group (command set GUID).
         /// </summary>
-        public static readonly Guid CommandSet = new Guid("222993a5-b14f-4206-9ba3-00cf383c7d99");
+        public static readonly Guid s_commandSet = new Guid("222993a5-b14f-4206-9ba3-00cf383c7d99");
 
         /// <summary>
         /// VS Package that provides this command, not null.
         /// </summary>
-        private readonly AsyncPackage package;
+        private readonly AsyncPackage _package;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="FindHandlerCommand"/> class.
@@ -46,11 +52,11 @@ namespace HandlerFinder
         /// <param name="commandService">Command service to add command to, not null.</param>
         private FindHandlerCommand(AsyncPackage package, OleMenuCommandService commandService)
         {
-            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            _package = package ?? throw new ArgumentNullException(nameof(package));
             commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
 
-            var menuCommandID = new CommandID(CommandSet, CommandId);
-            var menuItem = new OleMenuCommand(this.Execute, menuCommandID);
+            var menuCommandID = new CommandID(s_commandSet, CommandId);
+            var menuItem = new OleMenuCommand(Execute, menuCommandID);
             menuItem.BeforeQueryStatus += MenuItem_BeforeQueryStatus;
             commandService.AddCommand(menuItem);
         }
@@ -68,7 +74,7 @@ namespace HandlerFinder
                 command.Visible = false;
                 ThreadHelper.JoinableTaskFactory.Run(async delegate
                 {
-                    (var node, var document) = await GetCurrentTokenAndDocumentAsync();
+                    (SyntaxNode node, Document document) = await GetCurrentTokenAndDocumentAsync();
                     command.Visible = IsSyntaxNodeSupported(node);
                 });
             }
@@ -86,11 +92,11 @@ namespace HandlerFinder
         /// <summary>
         /// Gets the service provider from the owner package.
         /// </summary>
-        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        private IAsyncServiceProvider ServiceProvider
         {
             get
             {
-                return this.package;
+                return _package;
             }
         }
 
@@ -117,39 +123,217 @@ namespace HandlerFinder
         /// <param name="e">Event args.</param>
         private void Execute(object sender, EventArgs e)
         {
+            string requestedCommandOrRequest = string.Empty;
+            (string fileName, int lineToGoTo) = (string.Empty, 0);
+
             ThreadHelper.ThrowIfNotOnUIThread();
 
             ThreadHelper.JoinableTaskFactory.Run(async delegate
             {
-                (var node, var document) = await GetCurrentTokenAndDocumentAsync();
+                (SyntaxNode node, Document document) = await GetCurrentTokenAndDocumentAsync();
 
                 if (IsSyntaxNodeSupported(node))
                 {
-                    SemanticModel semanticModel = await document.GetSemanticModelAsync();
-
-                    var symbolInfo = semanticModel.GetSymbolInfo(node);
-                    var typeInfo = semanticModel.GetTypeInfo(node);
-
-                    var symbol = semanticModel.GetDeclaredSymbol(node);
-                    
-                    var containingNamespace = symbol.ContainingNamespace;
-
-                    System.Windows.Forms.Clipboard.SetText(symbol.ToDisplayString(SymbolDisplayFormat.CSharpErrorMessageFormat));
+                    requestedCommandOrRequest = GetIdentifierNameByNode(node);
                 }
-
             });
 
-            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
-            string title = "FindHandlerCommand";
+            if (string.IsNullOrWhiteSpace(requestedCommandOrRequest))
+            {
+                return;
+            }
 
-            // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.package,
-                message,
-                title,
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            ThreadHelper.JoinableTaskFactory.Run(async delegate
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                if (requestedCommandOrRequest.ToLowerInvariant().EndsWith("request"))
+                {
+                    (fileName, lineToGoTo) =
+                        await FindRequestHandlerAsync(requestedCommandOrRequest);
+                }
+                else if (requestedCommandOrRequest.ToLowerInvariant().EndsWith("command"))
+                {
+                    (fileName, lineToGoTo) =
+                        await FindCommandHandlerAsync(requestedCommandOrRequest);
+                }
+            });
+
+            if (!string.IsNullOrEmpty(fileName))
+            {
+                var dte = Package.GetGlobalService(typeof(_DTE)) as DTE2;
+                dte.ExecuteCommand("File.OpenFile", fileName);
+
+                if (lineToGoTo > 0)
+                {
+                    ((TextSelection)dte.ActiveDocument.Selection).GotoLine(lineToGoTo + 1);
+                }
+            }
+        }
+
+        private string GetIdentifierNameByNode(SyntaxNode node)
+        {
+            string name = string.Empty;
+
+            switch (node)
+            {
+                case RecordDeclarationSyntax _:
+                    name = ((BaseTypeDeclarationSyntax)node).Identifier.Text;
+                    break;
+                case IdentifierNameSyntax _:
+                    name = ((IdentifierNameSyntax)node).Identifier.Text;
+                    break;
+            }
+
+            if (name != "var")
+            {
+                return name;
+            }
+
+            return string.Empty;
+        }
+
+        private async Task<Tuple<string, int>> FindCommandHandlerAsync(string requestedCommandOrRequest)
+        {
+            string fileNameToOpen = string.Empty;
+            int lineToGoTo = 0;
+
+            var componentModel = (IComponentModel)await ServiceProvider.GetServiceAsync(typeof(SComponentModel));
+
+            if (componentModel == null)
+            {
+                return new Tuple<string, int>(string.Empty, 0);
+            }
+
+            VisualStudioWorkspace workspace = componentModel.GetService<VisualStudioWorkspace>();
+
+            Solution solution = workspace.CurrentSolution;
+
+            Project domainProject =
+                solution.Projects.SingleOrDefault(y => y.Name.ToLowerInvariant().EndsWith("domain"));
+
+            IEnumerable<GenericNameSyntax> types =
+                domainProject
+                .Documents
+                .Where(x => x.FilePath.ToLowerInvariant().Contains("commandhandlers"))
+                .Select(doc =>
+                    new
+                    {
+                        Model = doc.GetSemanticModelAsync().Result,
+                        Declarations = doc.GetSyntaxRootAsync().Result
+                            .DescendantNodes().OfType<GenericNameSyntax>()
+                    }).Where(x => x.Declarations.Any())
+                    .SelectMany(x => x.Declarations)
+                    .Where(x => x.Identifier.Text == "IRequestHandler");
+
+            foreach (GenericNameSyntax type in types)
+            {
+                foreach (TypeSyntax typeArgument in type.TypeArgumentList.Arguments)
+                {
+                    var typeArgumentIdentifierSyntax = (IdentifierNameSyntax)typeArgument;
+                    if (typeArgumentIdentifierSyntax.Identifier.Text == requestedCommandOrRequest)
+                    {
+                        fileNameToOpen = type.SyntaxTree.FilePath;
+
+                        Document document = domainProject.Documents.Single(x => x.FilePath == fileNameToOpen);
+
+                        SyntaxTree syntaxTree = await document.GetSyntaxTreeAsync();
+                        SyntaxNode semantics = await document.GetSyntaxRootAsync();
+
+                        IEnumerable<MethodDeclarationSyntax> declaredMethods = semantics.DescendantNodesAndSelf().OfType<MethodDeclarationSyntax>();
+
+                        foreach (MethodDeclarationSyntax method in declaredMethods)
+                        {
+                            if (((IdentifierNameSyntax)(method.ParameterList.Parameters.First()).Type).Identifier.Text == requestedCommandOrRequest)
+                            {
+                                lineToGoTo = syntaxTree.GetLineSpan(method.Span).StartLinePosition.Line;
+                                break;
+                            }
+                        }
+
+                        break;
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(fileNameToOpen))
+                {
+                    break;
+                }
+            }
+
+            return new Tuple<string, int>(fileNameToOpen, lineToGoTo);
+        }
+
+        private async Task<Tuple<string, int>> FindRequestHandlerAsync(string requestedCommandOrRequest)
+        {
+            string fileNameToOpen = string.Empty;
+            int lineToGoTo = 0;
+
+            var componentModel = (IComponentModel)await ServiceProvider.GetServiceAsync(typeof(SComponentModel));
+
+            if (componentModel == null)
+            {
+                return new Tuple<string, int>(string.Empty, 0);
+            }
+
+            VisualStudioWorkspace workspace = componentModel.GetService<VisualStudioWorkspace>();
+
+            Solution solution = workspace.CurrentSolution;
+
+            Project applicationProject =
+                solution.Projects.SingleOrDefault(y => y.Name.ToLowerInvariant().EndsWith("application"));
+
+            IEnumerable<GenericNameSyntax> types =
+                applicationProject
+                .Documents
+                .Where(x => x.FilePath.ToLowerInvariant().Contains("requesthandlers"))
+                .Select(doc =>
+                    new
+                    {
+                        Model = doc.GetSemanticModelAsync().Result,
+                        Declarations = doc.GetSyntaxRootAsync().Result
+                            .DescendantNodes().OfType<GenericNameSyntax>()
+                    }).Where(x => x.Declarations.Any())
+                    .SelectMany(x => x.Declarations)
+                    .Where(x => x.Identifier.Text == "IRequestHandler");
+
+            foreach (GenericNameSyntax type in types)
+            {
+                foreach (TypeSyntax typeArgument in type.TypeArgumentList.Arguments)
+                {
+                    var typeArgumentIdentifierSyntax = (IdentifierNameSyntax)typeArgument;
+                    if (typeArgumentIdentifierSyntax.Identifier.Text == requestedCommandOrRequest)
+                    {
+                        fileNameToOpen = type.SyntaxTree.FilePath;
+
+                        Document document = applicationProject.Documents.Single(x => x.FilePath == fileNameToOpen);
+
+                        SyntaxTree syntaxTree = await document.GetSyntaxTreeAsync();
+                        SyntaxNode semantics = await document.GetSyntaxRootAsync();
+
+                        IEnumerable<MethodDeclarationSyntax> declaredMethods = 
+                            semantics.DescendantNodesAndSelf().OfType<MethodDeclarationSyntax>();
+
+                        foreach (MethodDeclarationSyntax method in declaredMethods)
+                        {
+                            if (((IdentifierNameSyntax)(method.ParameterList.Parameters.First()).Type).Identifier.Text == requestedCommandOrRequest)
+                            {
+                                lineToGoTo = syntaxTree.GetLineSpan(method.Span).StartLinePosition.Line;
+                                break;
+                            }
+                        }
+
+                        break;
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(fileNameToOpen))
+                {
+                    break;
+                }
+            }
+
+            return new Tuple<string, int>(fileNameToOpen, lineToGoTo);
         }
 
         /// <summary>
@@ -159,8 +343,12 @@ namespace HandlerFinder
         /// <returns></returns>
         private bool IsSyntaxNodeSupported(SyntaxNode node)
         {
-            return node != null
-                && ((node is MemberDeclarationSyntax));
+            bool isSupported = node != null
+                && ((node is IdentifierNameSyntax) || (node is RecordDeclarationSyntax));
+
+            isSupported &= GetIdentifierNameByNode(node) != string.Empty;
+
+            return isSupported;
         }
 
         /// <summary>
@@ -172,7 +360,7 @@ namespace HandlerFinder
             IWpfTextView textView = await GetTextViewAsync();
             SnapshotPoint caretPosition = textView.Caret.Position.BufferPosition;
             Document document = caretPosition.Snapshot.GetOpenDocumentInCurrentContextWithChanges();
-            var syntaxRoot = await document.GetSyntaxRootAsync();
+            SyntaxNode syntaxRoot = await document.GetSyntaxRootAsync();
             return (syntaxRoot.FindToken(caretPosition).Parent, document);
         }
 
@@ -187,7 +375,7 @@ namespace HandlerFinder
             Assumes.Present(textManager);
             textManager.GetActiveView(1, null, out IVsTextView textView);
 
-            var editorAdaptersFactoryService = await GetEditorAdaptersFactoryServiceAsync();
+            IVsEditorAdaptersFactoryService editorAdaptersFactoryService = await GetEditorAdaptersFactoryServiceAsync();
 
             return editorAdaptersFactoryService.GetWpfTextView(textView);
         }
